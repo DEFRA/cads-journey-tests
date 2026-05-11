c19f5a211e2c37318b9d815729d566f497d67aee
Craig John <craig.john@esynergy.co.uk>
2026-05-11 14:37:17 +0100

    page timeout

--- tests/fixtures/setup.ts
+++ tests/fixtures/setup.ts
@@ -3,6 +3,7 @@ import { test as setup } from './test.fixture'
 const authFile = 'playwright/.auth/user.json'
 
 setup('authenticate', async ({ page, loginPageStepDefinitions }) => {
+  page.setDefaultNavigationTimeout(60000)
   await loginPageStepDefinitions.INaviagteToLoginPage()
   await loginPageStepDefinitions.ILoginWithValidCredentials(
     process.env.CADS_MIP_FRONTEND_USERNAME!,
