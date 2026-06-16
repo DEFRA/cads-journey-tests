5a127e96c6f612e379d38c2dcdf85a521bcb4376
Craig John <craig.john@esynergy.co.uk>
2026-06-16 14:31:44 +0100

    check authentication

--- tests/fixtures/setup.ts
+++ tests/fixtures/setup.ts
@@ -2,12 +2,15 @@ import { test as setup } from './test.fixture'
 
 const authFile = 'playwright/.auth/user.json'
 
-setup('authenticate', async ({ page, loginPageStepDefinitions }) => {
-  await loginPageStepDefinitions.INaviagteToLoginPage()
-  await loginPageStepDefinitions.ILoginWithValidCredentials(
-    process.env.CADS_MIP_FRONTEND_USERNAME!,
-    process.env.CADS_MIP_FRONTEND_PASSWORD!
-  )
-
-  await page.context().storageState({ path: authFile })
-})
+setup(
+  'authenticate',
+  async ({ page, loginPageStepDefinitions, dashboardPageStepDefinitions }) => {
+    await loginPageStepDefinitions.INaviagteToLoginPage()
+    await loginPageStepDefinitions.ILoginWithValidCredentials(
+      process.env.CADS_MIP_FRONTEND_USERNAME!,
+      process.env.CADS_MIP_FRONTEND_PASSWORD!
+    )
+    await dashboardPageStepDefinitions.IHaveLandedOnTheDashboardPage()
+    await page.context().storageState({ path: authFile })
+  }
+)
