90ede244d1fcf7ad7e23f1ecfef752ef8ac2e426
Craig John <craig.john@esynergy.co.uk>
2026-08-03 15:51:28 +0100

    setup debug step

--- tests/fixtures/setup.ts
+++ tests/fixtures/setup.ts
@@ -4,6 +4,10 @@ import { test as setup } from './test.fixture'
 setup(
   'authenticate',
   async ({ page, loginPageStepDefinitions, dashboardPageStepDefinitions }) => {
+    await setup.step('getLocationsWithCPHAndModifiedDate', async () => {
+      console.info('EVV: ' + process.env.ENVIRONMENT)
+      console.info('API: ' + process.env.CADS_MIP_FRONTEND_BASE_URL)
+    })
     await loginPageStepDefinitions.INaviagteToLoginPage()
     await loginPageStepDefinitions.ILoginWithValidCredentials(
       process.env.CADS_MIP_FRONTEND_USERNAME!,
