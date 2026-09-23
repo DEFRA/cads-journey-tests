b08346e3c421778239ecf66f090ace9115a97b92
Craig John <craig.john@esynergy.co.uk>
2026-09-23 09:14:06 +0100

    graphql utils

--- tests/fixtures/backend.fixture.ts
+++ tests/fixtures/backend.fixture.ts
@@ -1,11 +1,13 @@
 import { test as base, request } from '@playwright/test'
 import { HealthApiStepDefinitions } from '../back-end/step-definitions/health.api.step.definitions'
 import { LocationspiStepDefinitions } from '../back-end/step-definitions/locations.api.step.definitions'
+import { GraphQlApiStepDefinitions } from '../back-end/step-definitions/graphQL.api.step.definitions'
 
 // Declare the types of your fixtures.
 type ApiStepDefinitions = {
   healthApiStepDefinitions: HealthApiStepDefinitions
   locationsApiStepDefinitions: LocationspiStepDefinitions
+  graphqlApiStepDefinitions: GraphQlApiStepDefinitions
 }
 
 // Extend base test by providing "healthApiStepDefinitions" fixture.
@@ -33,5 +35,13 @@ export const backendTests = base.extend<ApiStepDefinitions>({
       apiContext
     )
     await use(locationsApiStepDefinitions)
+  },
+  // eslint-disable-next-line no-empty-pattern
+  graphqlApiStepDefinitions: async ({}, use) => {
+    const apiContext = await request.newContext({
+      baseURL: process.env.CADS_CDS_BACKEND_BASE_URL!
+    })
+    const graphQlApiStepDefinitions = new GraphQlApiStepDefinitions(apiContext)
+    await use(graphQlApiStepDefinitions)
   }
 })
--- tests/utils/enums.ts
+++ tests/utils/enums.ts
@@ -4,10 +4,17 @@ export enum EndPoints {
 }
 
 export enum Queries {
-  SelectAllLocationsIdentifiers = "SELECT *, TO_CHAR(lid_current_modified_date, 'YYYY-MM-DD') AS lid_current_modified_date FROM cts.ct_location_identifiers;",
-  SelectAllLocations = "SELECT *, TO_CHAR(loc_current_modified_date, 'YYYY-MM-DD') AS loc_current_modified_date, TO_CHAR(loc_effective_to, 'YYYY-MM-DD') AS loc_effective_to FROM cts.ct_locations;"
+  SelectAllLocationsIdentifiers = 'select * from cts.ct_location_identifiers',
+  SelectAllLocations = "SELECT *, TO_CHAR(loc_current_modified_date, 'YYYY-MM-DD') AS loc_current_modified_date FROM cts.ct_locations;",
+  SelectAllFileImports = 'select * from cads.cts_file_imports;',
+  SelectAllTransactions = 'select * from cts_transactions.ct_locations;'
 }
 
 export enum AuthFile {
   User = 'playwright/.auth/user.json'
+}
+
+export enum FileNames {
+  Path = 'data/',
+  BulkUpload0001 = 'CTSM_CADS_PREP_BULK_00001_001_CT_LOCATIONS_2026-07-28-094638.csv'
 }
