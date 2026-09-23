34aa06d7255f20394473fce0eaae0cc7472bd408
Craig John <craig.john@esynergy.co.uk>
2026-09-23 15:01:48 +0100

    lint fixes

--- tests/back-end/step-definitions/locations.api.step.definitions.ts
+++ tests/back-end/step-definitions/locations.api.step.definitions.ts
@@ -54,6 +54,7 @@ export class LocationspiStepDefinitions {
 
   async getLocationsWithCPHAndModifiedDate() {
     const { cph, modifiedDate } = await this.getExpectedCPH()
+    const l = await this.getLocations();
     const lastModifiedDate = this.cadsDataService.isCDPEnvironment
       ? modifiedDate
       : (await this.getLocations()).filter(
